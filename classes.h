8f98d4b28130e1971e5866547e9d4d8cdba8aa31
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-11-20 20:25:09 +0530

    feat: add learning rate property to the neural net class feat: add more arguments for weight derivate feat: add Epochs property to backpropogate refacotr: generalize the name of getWeightTCostDerivative to a also work for bias

--- classes.h
+++ classes.h
@@ -31,16 +31,17 @@ class NeuralNet{
     int* hidOutLayerSizes;
     int inputLayerSize;
     int outputLayerSize;
+    float lRate;
     public:
-    NeuralNet(int inputLayerSize, int hidOutLayerCount, int* hidOutLayerSizes, int outputLayerSize);
+    NeuralNet(int inputLayerSize, int hidOutLayerCount, int* hidOutLayerSizes, int outputLayerSize, float lRate);
     ~NeuralNet();
     void describe();
     void resetNeuronsActivations();
     float* feedForward(float* inputs, int inputSize);
     void predict(float* inputs, int inputSize);
     float cost(float* target, int targetArr_size);
-    float getWeightTCostDerivative(float& weight,float* targetArr, int targetArr_size);
-    void backPropogate(float* inputArr, int inputSize, float* target, int targetArr_size);
+    float getParamTCostDerivative(float& param, float* inputArr, int inputSize, float* targetArr, int targetArr_size);
+    void backPropogate(float* inputArr, int inputSize, float* target, int targetArr_size, int epochs);
 };
 
 #endif
