708a35b6dd619624ef7e73969b27cd9799a7d885
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-03-13 21:32:33 +0530

    test(api): add demo instructions to test the mlp api

--- try1 (OOP Approach)/rust/src/main.rs
+++ try1 (OOP Approach)/rust/src/main.rs
@@ -1,5 +1,13 @@
 mod mlp;
 fn main() {
-    let layer = mlp::Layer::new(65000, 20);
-    layer.show_neurons();
+    let mut layers: Vec<u16> = Vec::new();
+    let mut inputs: Vec<f32> = Vec::new();
+    layers.push(40);
+    layers.push(20);
+    inputs.push(0.12);
+    inputs.push(0.42);
+    inputs.push(0.11);
+    let mut mlp = mlp::Mlp::new(10, &layers, 0.03);
+    mlp.describe();
+    mlp.feed_forward(&inputs);
 }
