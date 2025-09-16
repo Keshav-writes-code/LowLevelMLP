800af4bbb4d0b2bfb9e75d314d8c56e660ff1491
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-09-16 22:54:34 +0530

    feat(mlp)!: internalize predictions and change feedForward to void to support stateful outputs

--- try1 (OOP Approach)/cpp/include/NeuralNet/classes.h
+++ try1 (OOP Approach)/cpp/include/NeuralNet/classes.h
@@ -1,55 +1,57 @@
 #ifndef CLASSES_H
 #define CLASSES_H
 
-namespace NeuralNet
-{
-  class Neuron
-  {
-  public:
-    float value;
-    float bias;
-    float *weights;
-    int prevLayerNeurons_count;
-    Neuron(int prevLayerNeurons_count);
-    ~Neuron();
-  };
+namespace NeuralNet {
+class Neuron {
+public:
+  float value;
+  float bias;
+  float *weights;
+  int prevLayerNeurons_count;
+  Neuron(int prevLayerNeurons_count);
+  ~Neuron();
+};
 
-  class Layer
-  {
-  private:
-    int size;
+class Layer {
+private:
+  int size;
 
-  public:
-    Neuron **neurons;
-    Layer(int size, int prevLayerSize);
-    ~Layer();
-    void showNeurons();
-  };
+public:
+  Neuron **neurons;
+  Layer(int size, int prevLayerSize);
+  ~Layer();
+  void showNeurons();
+};
 
-  class MLP
-  {
-  private:
-    Layer **HidOutlayers;
-    int hidOutLayerCount;
-    int *hidOutLayerSizes;
-    int inputLayerSize;
-    int outputLayerSize;
-    float lRate;
+class MLP {
+private:
+  Layer **HidOutlayers;
+  int hidOutLayerCount;
+  int *hidOutLayerSizes;
+  int inputLayerSize;
+  int outputLayerSize;
+  float lRate;
+  float *predictions;
 
-  public:
-    MLP(int inputLayerSize, int hidOutLayerCount, int *hidOutLayerSizes, int outputLayerSize, float lRate);
-    ~MLP();
-    void describe();
-    void resetNeuronsActivations();
-    float *feedForward(float *inputs, int inputSize);
-    void predict(float **inputs, int inputSize, float **target, int targetSize, int samplesCount);
-    float cost(float *target, int targetArr_size);
-    float getParamTCostDerivative(float &param, float *inputArr, int inputSize, float *targetArr, int targetArr_size);
-    void backPropogate(float *inputArr, int inputSize, float *target, int targetArr_size);
-    void train(float **inputArr_2d, int input_elem_size, float **targetArr_2d, int target_elem_size, int items_count, int epochs);
-    void printParamsCount();
-    void constructLayer(int i);
-  };
-}
+public:
+  MLP(int inputLayerSize, int hidOutLayerCount, int *hidOutLayerSizes,
+      int outputLayerSize, float lRate);
+  ~MLP();
+  void describe();
+  void resetNeuronsActivations();
+  void feedForward(float *inputs, int inputSize);
+  void predict(float **inputs, int inputSize, float **target, int targetSize,
+               int samplesCount);
+  float cost(float *target, int targetArr_size);
+  float getParamTCostDerivative(float &param, float *inputArr, int inputSize,
+                                float *targetArr, int targetArr_size);
+  void backPropogate(float *inputArr, int inputSize, float *target,
+                     int targetArr_size);
+  void train(float **inputArr_2d, int input_elem_size, float **targetArr_2d,
+             int target_elem_size, int items_count, int epochs);
+  void printParamsCount();
+  void constructLayer(int i);
+};
+} // namespace NeuralNet
 
 #endif
